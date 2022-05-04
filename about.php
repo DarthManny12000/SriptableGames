15088915a200dd80933db700bd4fb5cf4db93ffc
DarthManny12000 <99301237+DarthManny12000@users.noreply.github.com>
2022-05-04 19:39:15 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Scriptable Games | About</title>
+
+<?php include "includes/include-header.php"; ?>
+
+	</head>
+	<body>
+
+<?php include "includes/include-navbar.php"; ?>
+
+		<div class="display-6">
+			&nbsp;
+		</div>
+
+		<div class="py-5 display-3 text-center hero-title">
+			ABOUT SCRIPTABLE GAMES
+		</div>
+		<div class="container">
+			This is the offical Scriptable Games website made by Darth Manny which is me I am the owner of this site but many other people helped in the creation of this site the reason I made this site is I love the internet and I want to make sites that people enjoy and I hope you enjoyed your stay here pls tell me what you think of this and have a nice day
+		</div>
+	</body>
+</html>    
